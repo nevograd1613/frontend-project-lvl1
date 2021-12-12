6d402e909c1f3c7a3c1087618d4940f0b82ecbe1
Данила <golubev.d.a@mail.ru>
2021-12-12 20:49:52 +0300

    lint fix

--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ const playGame = (createRound, description) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
-  console.log('${description}');
+  console.log(description);
 
-const numberOfRounds = 3;
+  const numberOfRounds = 3;
 
   for (let i = 0; i < numberOfRounds; i += 1) {
     const [correctAnswer, quest] = createRound();
     console.log(`Question: ${quest}`);
     const userAnswer = readlineSync.question('Your answer:');
-  
     if (userAnswer !== correctAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \n Let's try again, ${userName}!`);
     }
