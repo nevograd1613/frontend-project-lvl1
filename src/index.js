6a7747231dffe48c3186351384485cdbe682be0e
Danila <golubev.d.a@mail.ru>
2022-12-09 09:40:09 +0300

    fix bags

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const playGame = (createRound, description) => {
     console.log(`Question: ${quest}`);
     const userAnswer = readlineSync.question('Your answer:');
     if (userAnswer !== correctAnswer) {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \n Let's try again, ${userName}!`);
+      return console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \n Let's try again, ${userName}!`);
     }
     console.log('Correct!');
   }
